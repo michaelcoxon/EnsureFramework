231577ad2dfc7f687ece6ce7edb666178f0731b1
Michael Coxon <michaelcoxon@outlook.com.au>
2018-06-15 19:06:51 +0800

    added more tests

---
+++ EnsureFramework.UnitTests/Assertions/GuidAssertionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EnsureFramework.UnitTests.Assertions
+{
+    public class GuidAssertionsTests
+    {
+        [Fact]
+        public void IsValidGuidTest()
+        {
+            Ensure.Arg(Guid.NewGuid(), "guid").IsValidGuid();
+        }
+
+        [Fact]
+        public void IsValidGuidFailTest()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Ensure.Arg(Guid.Empty, "guid").IsValidGuid();
+            });
+        }
+
+        [Fact]
+        public void IsValidGuidNullableTest()
+        {
+            Guid? guid = Guid.NewGuid();
+
+            Ensure.Arg(guid, "guid").IsValidGuid();
+        }
+
+        [Fact]
+        public void IsValidGuidNullableEmptyFailTest()
+        {
+            Guid? guid = Guid.Empty;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Ensure.Arg(guid, "guid").IsValidGuid();
+            });
+        }
+
+        [Fact]
+        public void IsValidGuidNullableNullFailTest()
+        {
+            Guid? guid = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                Ensure.Arg(guid, "guid").IsValidGuid();
+            });
+        }
+    }
+}
