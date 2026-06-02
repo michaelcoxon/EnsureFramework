deb4d1afed6f8ec3bc6bdc6980e0b529b083557e
Michael Coxon <michaelccoxon@gmail.com>
2026-06-02 22:27:02 +0800

    add IsNegative, add IsZero

--- EnsureFramework.UnitTests/NumberAssertionsTests.cs
+++ EnsureFramework.UnitTests/NumberAssertionsTests.cs
@@ -29,6 +29,24 @@ namespace EnsureFramework.UnitTests
         }
 
         [Fact]
+        public void IsNegative_Positive_Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Ensure.Arg(1, "value").IsNegative());
+        }
+
+        [Fact]
+        public void IsNegative_Zero_Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Ensure.Arg(0, "value").IsNegative());
+        }
+
+        [Fact]
+        public void IsNegative_Negative_Test()
+        {
+            Ensure.Arg(-1, "value").IsNegative();
+        }
+
+        [Fact]
         public void IsNotZero_Positive_Test()
         {
             Ensure.Arg(1, "value").IsNotZero();
@@ -44,6 +62,24 @@ namespace EnsureFramework.UnitTests
         public void IsNotZero_Negative_Test()
         {
             Ensure.Arg(-1, "value").IsNotZero();
+        }
+
+        [Fact]
+        public void IsZero_Positive_Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Ensure.Arg(1, "value").IsZero());
+        }
+
+        [Fact]
+        public void IsZero_Zero_Test()
+        {
+            Ensure.Arg(0, "value").IsZero();
+        }
+
+        [Fact]
+        public void IsZero_Negative_Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Ensure.Arg(-1, "value").IsZero());
         }
 
         [Fact]
--- EnsureFramework/NumberAssertions.cs
+++ EnsureFramework/NumberAssertions.cs
@@ -26,11 +26,33 @@ namespace EnsureFramework
             if (T.IsNegative(@this.Argument))
             {
                 throw new ArgumentOutOfRangeException(
-                    @this.ArgumentName, 
+                    @this.ArgumentName,
                     @this.Argument,
                     string.Format(Resources.Strings.The_argument_argName_is_a_negative_number_Format, @this.ArgumentName));
             }
             return @this.AssertionPassed();
+        }
+
+        /// <summary>
+        /// Asserts that the argument is a negative number.
+        /// </summary>
+        /// <remarks>Use this method to ensure that a numeric argument is negative before proceeding with
+        /// further logic. This method supports method chaining for additional assertions.</remarks>
+        /// <typeparam name="T">The numeric type of the argument.</typeparam>
+        /// <param name="this">The argument assertion builder containing the value to validate. Cannot be null.</param>
+        /// <returns>The same assertion builder instance if the argument is negative, enabling further assertions.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the argument is not a negative number.</exception>
+        [DebuggerNonUserCode]
+        public static IArgumentAssertionBuilder<T> IsNegative<T>([NotNull] this IArgumentAssertionBuilder<T> @this) where T : INumberBase<T>
+        {
+            if (T.IsNegative(@this.Argument))
+            {
+                return @this.AssertionPassed();
+            }
+            throw new ArgumentOutOfRangeException(
+                @this.ArgumentName,
+                @this.Argument,
+                string.Format(Resources.Strings.The_argument_argName_is_a_negative_number_Format, @this.ArgumentName));
         }
 
         /// <summary>
@@ -44,11 +66,35 @@ namespace EnsureFramework
             if (T.IsZero(@this.Argument))
             {
                 throw new ArgumentOutOfRangeException(
-                    @this.ArgumentName, 
+                    @this.ArgumentName,
                     @this.Argument,
                     string.Format(Resources.Strings.The_argument_argName_is_equal_to_zero_Format, @this.ArgumentName));
             }
             return @this.AssertionPassed();
+        }
+
+        /// <summary>
+        /// Asserts that the argument is zero.
+        /// </summary>
+        /// <remarks>Use this method to ensure that a numeric argument is exactly zero. This assertion is
+        /// type-safe for any numeric type.</remarks>
+        /// <typeparam name="T">The numeric type of the argument.</typeparam>
+        /// <param name="this">The argument assertion builder containing the value to check. Cannot be null.</param>
+        /// <returns>The current assertion builder if the argument is zero.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the argument is not zero.</exception>
+        [DebuggerNonUserCode]
+        public static IArgumentAssertionBuilder<T> IsZero<T>([NotNull] this IArgumentAssertionBuilder<T> @this) where T : INumberBase<T>
+        {
+            if (T.IsZero(@this.Argument))
+            {
+                return @this.AssertionPassed();
+            }
+
+            throw new ArgumentOutOfRangeException(
+                @this.ArgumentName,
+                @this.Argument,
+                string.Format(Resources.Strings.The_argument_argName_is_equal_to_zero_Format, @this.ArgumentName));
+
         }
     }
 }
