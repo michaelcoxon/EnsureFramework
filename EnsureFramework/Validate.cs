133ce1cc9c6fb3141d7b2cdc682d504b5ed27426
Michael Coxon <michaelccoxon@gmail.com>
2026-06-06 23:13:26 +0800

    add the start to validation

---
+++ EnsureFramework/Validate.cs
@@ -0,0 +1,68 @@
+﻿namespace EnsureFramework
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    using EnsureFramework.Results;
+
+    public static class Validate
+    {
+        public static IValidationResult<T> That<T>(T? source, [CallerArgumentExpression(nameof(source))] string? sourceName = null)
+        {
+            return new ValidationResult<T>
+            {
+                Name = sourceName,
+                Value = source
+            };
+        }
+    }
+
+    public interface IValidationResult<T>
+    {
+        T? Value { get; }
+        string? Name { get; }
+        List<IAssertionResult> AssertionResults { get; }
+        bool IsError { get; }
+        IEnumerable<string> Messages { get; }
+    }
+
+    public sealed class ValidationResult<T> : IValidationResult<T>
+    {
+        public T? Value { get; set; }
+        public string? Name { get; set; }
+        public List<IAssertionResult> AssertionResults { get; } = [];
+        public bool IsError => this.AssertionResults.Any(v => !v.Success);
+        public IEnumerable<string> Messages => this.AssertionResults.Where(v => !v.Success).Select(v => v.Message!);
+    }
+
+    public static class ObjectValidationExtensions
+    {
+        public static IValidationResult<T> IsNotNull<T>([NotNull] this IValidationResult<T> @this)
+        {
+            if (@this.Value is null)
+            {
+                @this.AssertionResults.Add(AssertionResult.Fail("Value should not be null."));
+            }
+            else
+            {
+                @this.AssertionResults.Add(AssertionResult.Ok);
+            }
+            return @this;
+        }
+
+        public static IValidationResult<T> IsNull<T>([NotNull] this IValidationResult<T> @this)
+        {
+            if (@this.Value is not null)
+            {
+                @this.AssertionResults.Add(AssertionResult.Fail("Value should be null."));
+            }
+            else
+            {
+                @this.AssertionResults.Add(AssertionResult.Ok);
+            }
+            return @this;
+        }
+    }
+}
