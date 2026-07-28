f09f801c234e702dd79ce29d99eacead669d8a96
Michael Coxon <michaelccoxon@gmail.com>
2026-07-29 00:21:24 +0800

    add dictionary assertion tests

---
+++ tests/EnsureFramework.UnitTests/AssertionTests/DictionaryAssertionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EnsureFramework;
+using EnsureFramework.Assertions;
+
+using Xunit;
+
+namespace EnsureFramework.UnitTests.AssertionTests
+{
+    public class DictionaryAssertionsTests
+    {
+        [Fact]
+        public void HasKeyTest()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                ["key"] = "value",
+            };
+            Assert.True(DictionaryAssertions.HasKey(dictionary, "key").Success);
+        }
+
+        [Fact]
+        public void HasKeyFailTest()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                ["key"] = "value",
+            };
+            Assert.False(DictionaryAssertions.HasKey(dictionary, "notkey").Success);
+        }
+    }
+}
